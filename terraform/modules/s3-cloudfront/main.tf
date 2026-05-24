5c43c80822338c2c276633bbfdc094afca833b09
michael-rousseau <michael.rousseau@flowdesk.co>
2026-05-24 13:41:51 +0200

    fix: use S3 ACL instead of bucket policy for Learner Lab

--- terraform/modules/s3-cloudfront/main.tf
+++ terraform/modules/s3-cloudfront/main.tf
@@ -15,6 +15,13 @@ resource "aws_s3_bucket_website_configuration" "frontend" {
   }
 }
 
+resource "aws_s3_bucket_ownership_controls" "frontend" {
+  bucket = aws_s3_bucket.frontend.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
 resource "aws_s3_bucket_public_access_block" "frontend" {
   bucket                  = aws_s3_bucket.frontend.id
   block_public_acls       = false
@@ -23,20 +30,8 @@ resource "aws_s3_bucket_public_access_block" "frontend" {
   restrict_public_buckets = false
 }
 
-resource "aws_s3_bucket_policy" "frontend" {
+resource "aws_s3_bucket_acl" "frontend" {
   bucket     = aws_s3_bucket.frontend.id
-  depends_on = [aws_s3_bucket_public_access_block.frontend]
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Sid       = "PublicReadGetObject"
-        Effect    = "Allow"
-        Principal = "*"
-        Action    = "s3:GetObject"
-        Resource  = "${aws_s3_bucket.frontend.arn}/*"
-      }
-    ]
-  })
+  acl        = "public-read"
+  depends_on = [aws_s3_bucket_ownership_controls.frontend, aws_s3_bucket_public_access_block.frontend]
 }
